3152f3c07d93ac022fea62b00b0b8b885953287a
EaganOG <connelleaganwebb@gmail.com>
2024-09-03 16:07:19 +1200

    Integrated Swagger with the App, Nearly fully implimented except for a reference resolution issue 'Could not resolve reference: Could not resolve pointer: /components/schemas/user does not exist in document'. Unsure how to fix currently but looking in to it

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import the Express module
 import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 // Import the index routes module
 import indexRoutes from "./routes/index.js";
@@ -13,7 +15,36 @@ const PORT = process.env.PORT || 3000;
 
 // Use the routes module
 app.use("/", indexRoutes);
-app.use("/users", usersRoutes);
+app.use(express.urlencoded({ extended: false })); // To parse the incoming requests with urlencoded payloads. For example, form data
+
+// This should be declared under app.use(urlencoded({ extended: false }));
+app.use(express.json()); // To parse the incoming requests with JSON payloads. For example, REST API requests
+// This should be declared under app.use(express.json());
+const swaggerOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "User Management System API",
+        version: "0.0.1",
+        description: "A User Management System API",
+        contact: {
+          name: "Greedy Guppies",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+};
+  
+  // This should be declared under const swaggerOptions = { ... };
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api/users", usersRoutes);
+// This should be declared under app.use("/api/institutions", institutionRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Start the server on port 3000
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -10,10 +10,243 @@ import {
 
 const router = express.Router();
 
+
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/user'
+ *     responses:
+ *       '201':
+ *         description: User successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "User successfully created"
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/user'
+ *       '400':
+ *         description: User with the same name already exists
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "User with the same name already exists"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An unexpected error occurred"
+*/
 router.post("/", createUser);
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     tags:
+ *       - User
+ *     responses:
+ *       '200':
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/user'
+ *       '404':
+ *         description: No users found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No users found"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An unexpected error occurred"
+*/
 router.get("/", findUsers);
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Get an user by id
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user id
+ *     responses:
+ *       '200':
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/user'
+ *       '404':
+ *         description: No user found with the provided id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No user with the id: {id} found"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An unexpected error occurred"
+ */
 router.get("/:id", findUser);
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update an user by id
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/users'
+ *     responses:
+ *       '200':
+ *         description: User successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "User with the id: {id} successfully updated"
+ *                 data:
+ *                   $ref: '#/components/schemas/users'
+ *       '404':
+ *         description: No user found with the provided id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No user with the id: {id} found"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An unexpected error occurred"
+ */
 router.put("/:id", updateUser);
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Delete a user by id
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user id
+ *     responses:
+ *       '200':
+ *         description: User successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "User with the id: {id} successfully deleted"
+ *       '404':
+ *         description: No user found with the provided id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No user with the id: {id} found"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An unexpected error occurred"
+ */
 router.delete("/:id", deleteUser);
 
 export default router;
